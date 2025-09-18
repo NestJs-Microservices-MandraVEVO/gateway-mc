1183ee66528484a4f6792297807f0604cc65be0a
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-09-18 12:27:07 -0600

    routes and var

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,11 @@ async function bootstrap() {
   const logger = new Logger('Main-Gateway')
 
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api');
+  
   await app.listen(envs.port);
 
-  logger.log(`Gateway`)
+  logger.log(`Gateway is running on port ${envs.port}`)
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   constructor() {}
+
+
+  @Post()
+  createProduct() {
+    return 'Product created';
+  }
+
+  @Get()
+  getProducts() {
+    return 'List of products';
+  }
+
+  @Get(':id')
+  getProductById(@Param('id') id: string) {
+    return `Product details for ${id}`;
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return `Product with ID ${id} deleted`;
+  }
+
+  @Patch(':id')
+  updateProduct(@Param('id') id: string,
+  @Body() body: any
+) {
+    return `Product with ID ${id} updated`;
+  }
 }
