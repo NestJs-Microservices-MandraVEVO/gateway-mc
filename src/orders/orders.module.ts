85372519faa56ec5d973e4a9cd6a4de0829434ff
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-12-13 15:42:01 -0600

    Implementacion de modulo NATS para la comunicacion entre servicios sin tener que repetir codigo en cada modulo

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs } from 'src/config/envs';
-import { NATS_SERVICE, ORDER_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [OrdersController],
   providers: [],
   imports: [
-    ClientsModule.register([
-          {
-            name: NATS_SERVICE,
-            transport: Transport.NATS,
-            options:{
-              servers: envs.natsServers
-            }
-          },
-        ]),
+    NatsModule
   ],
 })
 export class OrdersModule {}
