9d70031b809ba1db57822532b492ddb5f0956a99
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-12-17 14:47:08 -0600

    Add console log statement- Hola papu, primer cambio

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap() {
   
   await app.listen(envs.port);
 
+  console.log('hola papu, primer cambio');
+
   logger.log(`Gateway is running on port ${envs.port}`)
 }
 bootstrap();
